44636aa6d940f2f83d3df7eb08494eafc1e51d62
MrMatiz2 <alejandromatiz87@gmail.com>
2024-11-23 20:40:31 -0500

    Correction on configuration

--- src/main/java/com/example/tacocloudauthorizationserver/configuration/AuthorizationServerConfig.java
+++ src/main/java/com/example/tacocloudauthorizationserver/configuration/AuthorizationServerConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.oauth2.server.authorization.client.InMemoryR
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -36,6 +37,7 @@ public class AuthorizationServerConfig {
     @Bean
     public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
         OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+        http.getConfigurer(OAuth2AuthorizationServerConfigurer.class).oidc(Customizer.withDefaults());
         return http.formLogin(Customizer.withDefaults()).build();
     }
 
